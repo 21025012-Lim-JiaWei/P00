11efc22f00efe8b99af2572c2b76d5c0fd89f19b
21025012 <21025012@LAPTOP-INSHI9FT>
2022-05-30 15:18:26 +0800

    First commit to P00

---
+++ P00/src/Calculator01.java
@@ -0,0 +1,34 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21025012, 30 May 2022 11:37:38 am
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21025012, 30 May 2022 11:37:38 am
+ */
+
+public class Calculator01 {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	
+	public int div(int a, int b) {
+		return a / b;
+	}
+}
---
+++ P00/test/Calculator01Test.java
@@ -0,0 +1,100 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21025012, 30 May 2022 11:43:43 am
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21025012, 30 May 2022 11:43:43 am
+ */
+
+public class Calculator01Test {
+int a, b;
+Calculator01 cal;
+
+	@Before
+	public void setUp() throws Exception {
+		a = 4321;
+		b = 1234;
+		cal = new Calculator01();
+
+	}
+
+	@Test
+	// Unit test for addition
+	public void testadd() {
+		
+
+		int actual = cal.add(a,b);
+		int expected = 5555;
+		assertEquals(expected,actual);
+
+	}
+	
+	@Test
+	// Unit test for subtraction
+	public void testsub() {
+		
+		// Arrange 
+		int a = 4321;
+		int b = 1234;
+		Calculator01 cal = new Calculator01();
+		
+		// Actual
+		int actual = cal.sub(a,b);
+		int expected = 3087;
+		
+		// Assertion
+		assertEquals(expected,actual);
+	}
+	@Test
+	// Unit test for multiplication
+	public void testmul() {
+		
+		// Arrange
+		int a = 4321;
+		int b = 1234;
+		Calculator01 cal = new Calculator01();
+		
+		// Actual
+		int actual = cal.mul(a,b);
+		int expected = 5332114;
+		
+		// Assertion
+		assertEquals(expected,actual);
+	}
+	@Test
+	// Unit test for subtraction
+	public void testdiv() {
+		
+		// Arrange
+		int a = 4321;
+		int b = 1234;
+		Calculator01 cal = new Calculator01();
+		
+		// Actual
+		int actual = cal.div(a,b);
+		int expected = 3;
+		
+		// Assertion
+		assertEquals(expected,actual);
+	}
+	
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
